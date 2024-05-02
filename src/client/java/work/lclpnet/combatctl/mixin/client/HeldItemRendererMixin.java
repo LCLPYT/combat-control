f1fafbd629a84addfc95d41bbfff757ca64adb2e
LCLP <lukas@lclpnet.work>
2024-05-02 18:23:10 +0200

    remove attack cooldown equip animation when using old combat

--- src/client/java/work/lclpnet/combatctl/mixin/client/HeldItemRendererMixin.java
+++ src/client/java/work/lclpnet/combatctl/mixin/client/HeldItemRendererMixin.java
@@ -1,6 +1,9 @@
 package work.lclpnet.combatctl.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.item.HeldItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
@@ -10,6 +13,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import work.lclpnet.combatctl.api.CombatControlClient;
 import work.lclpnet.combatctl.impl.ItemInHandHandler;
 
 @Mixin(HeldItemRenderer.class)
@@ -24,5 +28,20 @@ public class HeldItemRendererMixin {
         if (ItemInHandHandler.onRenderHand(player, hand, item, matrices, vertexConsumers, light, tickDelta, pitch, swingProgress, equipProgress)) {
             ci.cancel();
         }
+    }
+
+    @WrapOperation(
+            method = "updateHeldItems",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/network/ClientPlayerEntity;getAttackCooldownProgress(F)F"
+            )
+    )
+    public float combatControl$removeCooldownEquipAnimation(ClientPlayerEntity instance, float v, Operation<Float> original) {
+        if (CombatControlClient.get().getAbilities().attackCooldown) {
+            return original.call(instance, v);
+        }
+
+        return 1;
     }
 }
