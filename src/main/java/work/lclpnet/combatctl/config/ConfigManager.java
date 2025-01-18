221c71079cad6745b4694793fa8e76b66ef73263
LCLP <lukas@lclpnet.work>
2025-01-18 13:50:00 +0100

    fix new config file serialization

--- src/main/java/work/lclpnet/combatctl/config/ConfigManager.java
+++ src/main/java/work/lclpnet/combatctl/config/ConfigManager.java
@@ -1,22 +1,25 @@
 package work.lclpnet.combatctl.config;
 
-import com.electronwill.nightconfig.core.Config;
-import com.electronwill.nightconfig.core.UnmodifiableConfig;
+import com.electronwill.nightconfig.core.CommentedConfig;
+import com.electronwill.nightconfig.core.UnmodifiableCommentedConfig;
 import com.electronwill.nightconfig.core.file.CommentedFileConfig;
 import com.electronwill.nightconfig.core.serde.ObjectDeserializer;
 import com.electronwill.nightconfig.core.serde.ObjectSerializer;
+import com.electronwill.nightconfig.core.serde.annotations.SerdeComment;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @ApiStatus.Internal
 public class ConfigManager<C> implements ConfigAccess<C>, AutoCloseable {
 
-    private final Path configPath;
     private final CommentedFileConfig fileConfig;
     private final ObjectSerializer serializer;
     private final ObjectDeserializer deserializer;
@@ -24,8 +27,17 @@ public class ConfigManager<C> implements ConfigAccess<C>, AutoCloseable {
     private @Nullable Runnable onChanged = null;
 
     public ConfigManager(Path configPath, C config) {
-        this.configPath = configPath;
         this.config = config;
+
+        Path dir = configPath.getParent();
+
+        if (!Files.exists(dir)) {
+            try {
+                Files.createDirectories(dir);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to create config directory", e);
+            }
+        }
 
         fileConfig = CommentedFileConfig.builder(configPath)
                 .autoreload()
@@ -42,18 +54,8 @@ public class ConfigManager<C> implements ConfigAccess<C>, AutoCloseable {
     }
 
     public synchronized void load() {
-        Path dir = configPath.getParent();
-
-        if (!Files.exists(dir)) {
-            try {
-                Files.createDirectories(dir);
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to create config directory", e);
-            }
-        }
-
-        var defaults = Config.inMemory();
-        serializer.serializeFields(config, defaults);
+        var defaults = CommentedConfig.inMemory();
+        updateDelegateConfig(defaults);
 
         fileConfig.load();
 
@@ -68,7 +70,7 @@ public class ConfigManager<C> implements ConfigAccess<C>, AutoCloseable {
 
     @Override
     public synchronized void save() {
-        updateFileConfig();
+        updateDelegateConfig(fileConfig);
         fileConfig.save();
     }
 
@@ -80,11 +82,12 @@ public class ConfigManager<C> implements ConfigAccess<C>, AutoCloseable {
         }
     }
 
-    private void updateFileConfig() {
-        serializer.serializeFields(config, fileConfig);
+    private void updateDelegateConfig(CommentedConfig cfg) {
+        serializer.serializeFields(config, cfg);
+        addEnumComments(cfg, config.getClass());
     }
 
-    private boolean addMissingEntries(UnmodifiableConfig src, Config dest) {
+    private boolean addMissingEntries(UnmodifiableCommentedConfig src, CommentedConfig dest) {
         boolean changed = false;
 
         for (var entry : src.entrySet()) {
@@ -94,16 +97,56 @@ public class ConfigManager<C> implements ConfigAccess<C>, AutoCloseable {
 
             if (destValue == null) {
                 dest.set(key, srcValue);
+                dest.setComment(key, src.getComment(key));
                 changed = true;
                 continue;
             }
 
-            if (srcValue instanceof UnmodifiableConfig nestedSrc && destValue instanceof Config nestedDest) {
+            if (srcValue instanceof UnmodifiableCommentedConfig nestedSrc && destValue instanceof CommentedConfig nestedDest) {
                 changed |= addMissingEntries(nestedSrc, nestedDest);
             }
         }
 
         return changed;
+    }
+
+    private void addEnumComments(CommentedConfig cfg, Class<?> type) {
+        for (var inst : ConfigOption.instanceTree(type)) {
+            var field = inst.option().field();
+            var fieldType = field.getType();
+
+            if (!fieldType.isEnum()) continue;
+
+            String path = Stream.concat(inst.srcPath().stream(), Stream.of(field))
+                    .map(Field::getName)
+                    .collect(Collectors.joining("."));
+
+            if (!cfg.contains(path)) continue;
+
+            StringBuilder longComment = new StringBuilder();
+            String comment = cfg.getComment(path);
+
+            if (comment != null) {
+                longComment.append(comment);
+                longComment.append('\n');
+            }
+
+            longComment.append("Possible options:");
+
+            for (Field enumOpt : fieldType.getFields()) {
+                longComment.append("\n    ");
+                longComment.append(enumOpt.getName());
+
+                String optComment = comment(enumOpt);
+
+                if (optComment != null) {
+                    longComment.append(": ");
+                    longComment.append(optComment);
+                }
+            }
+
+            cfg.setComment(path, longComment.toString());
+        }
     }
 
     @Override
@@ -115,5 +158,19 @@ public class ConfigManager<C> implements ConfigAccess<C>, AutoCloseable {
 
     public void onChanged(@Nullable Runnable action) {
         onChanged = action;
+    }
+
+    public static @Nullable String comment(Field field) {
+        SerdeComment[] comments = field.getDeclaredAnnotationsByType(SerdeComment.class);
+
+        if (comments.length == 0) return null;
+
+        var comment = new StringBuilder(comments[0].value());
+
+        for (int i = 1; i < comments.length; i++) {
+            comment.append("\n").append(comments[i].value());
+        }
+
+        return comment.toString();
     }
 }
