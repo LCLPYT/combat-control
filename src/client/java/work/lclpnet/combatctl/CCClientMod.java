861a2f632e017357612f60466686605193cac966
LCLP <lukas@lclpnet.work>
2025-02-02 16:04:08 +0100

    reset combat abilities upon joining a server
    closes #19

--- src/client/java/work/lclpnet/combatctl/CCClientMod.java
+++ src/client/java/work/lclpnet/combatctl/CCClientMod.java
@@ -2,7 +2,9 @@ package work.lclpnet.combatctl;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import work.lclpnet.combatctl.impl.CombatControlClientImpl;
+import work.lclpnet.combatctl.network.CombatAbilities;
 import work.lclpnet.combatctl.network.CombatControlClientNetworking;
 import work.lclpnet.kibu.config.ConfigManager;
 
@@ -16,9 +18,15 @@ public class CCClientMod implements ClientModInitializer {
 		var control = CombatControlClientImpl.get();
 		control.bind(configManager);
 
-		new CombatControlClientNetworking(control, CCModInit.LOGGER).init();
+		var networking = new CombatControlClientNetworking(control, CCModInit.LOGGER);
+		networking.init();
 
 		ClientLifecycleEvents.CLIENT_STOPPING.register(client
 				-> CCModInit.configManager().ifPresent(ConfigManager::close));
+
+		ClientPlayConnectionEvents.JOIN.register((networkHandler, sender, client) -> {
+			// reset combat abilities when joining another server
+			control.abilities().copy(new CombatAbilities());
+		});
 	}
 }
