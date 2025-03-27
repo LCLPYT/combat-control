a0ed1b191b24cdd91b4513ba85a0a9adbc486b56
LCLP <lukas@lclpnet.work>
2025-03-27 18:16:48 +0100

    fix notch apple effects not being applied

--- src/main/java/work/lclpnet/combatctl/mixin/LivingEntityMixin.java
+++ src/main/java/work/lclpnet/combatctl/mixin/LivingEntityMixin.java
@@ -4,47 +4,14 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import work.lclpnet.combatctl.api.CombatControl;
-import work.lclpnet.combatctl.config.PlayerConfig;
 import work.lclpnet.combatctl.impl.KnockbackHandler;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-
-    @Shadow
-    protected ItemStack activeItemStack;
-
-    @SuppressWarnings("ConstantValue")
-    @Inject(
-            method = "consumeItem",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/item/ItemStack;finishUsing(Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;)Lnet/minecraft/item/ItemStack;",
-                    shift = At.Shift.AFTER
-            )
-    )
-    protected void combatControl$completeUsingItem(CallbackInfo ci) {
-        if (!((Object) this instanceof ServerPlayerEntity player)) return;
-
-        PlayerConfig config = CombatControl.get(player.getServer()).playerConfig(player);
-
-        if (config.isModernNotchApple() || !activeItemStack.isOf(Items.ENCHANTED_GOLDEN_APPLE)) return;
-
-        player.removeStatusEffect(StatusEffects.ABSORPTION);
-        player.addStatusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 2400, 0));
-        player.addStatusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 600, 4));
-    }
 
     @WrapOperation(
             method = "takeKnockback",
