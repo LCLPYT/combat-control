29fbd687598a3a714797c91306961577b1926334
LCLP <lukas@lclpnet.work>
2025-04-07 19:11:03 +0200

    fix creative inventory dynamic items

--- src/main/java/work/lclpnet/combatctl/CCModInit.java
+++ src/main/java/work/lclpnet/combatctl/CCModInit.java
@@ -16,6 +16,7 @@ import work.lclpnet.combatctl.cmd.CombatCommand;
 import work.lclpnet.combatctl.cmd.ModTranslations;
 import work.lclpnet.combatctl.config.CombatControlConfig;
 import work.lclpnet.combatctl.impl.CombatControlImpl;
+import work.lclpnet.combatctl.impl.CreativeInventoryHandler;
 import work.lclpnet.combatctl.impl.PingHandler;
 import work.lclpnet.combatctl.impl.StaticCombatControl;
 import work.lclpnet.combatctl.network.CombatControlNetworking;
@@ -43,7 +44,6 @@ public class CCModInit implements ModInitializer {
 
 		var networking = new CombatControlNetworking(LOGGER);
 		networking.init();
-
 
 		ServerLifecycleEvents.SERVER_STARTING.register(server -> {
 			var control = new CombatControlImpl(server, configManager, networking);
@@ -74,6 +74,8 @@ public class CCModInit implements ModInitializer {
 				control.update(player);
 			}
 		});
+
+		new CreativeInventoryHandler().init();
 
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment)
 				-> new CombatCommand(translations, configManager).register(dispatcher));
---
+++ src/main/java/work/lclpnet/combatctl/impl/CreativeInventoryHandler.java
@@ -0,0 +1,54 @@
+package work.lclpnet.combatctl.impl;
+
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerSyncHandler;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import work.lclpnet.combatctl.mixin.ScreenHandlerAccessor;
+import work.lclpnet.kibu.hook.player.PlayerInventoryHooks;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CreativeInventoryHandler {
+
+    private final Set<Entry> entries = new HashSet<>();
+
+    public void init() {
+        // when putting items from the creative inventory into the player inventory, a de-sync can happen
+        // therefore, sync the slot at the end of the server tick
+
+        PlayerInventoryHooks.MODIFIED_CREATIVE_INVENTORY.register(event
+                -> entries.add(new Entry(event.player().networkHandler, event.slot())));
+
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            if (entries.isEmpty()) return;
+
+            for (var entry : entries) {
+                ServerPlayerEntity player = entry.handler.player;
+
+                if (player == null) continue;
+
+                ScreenHandler screenHandler = player.playerScreenHandler;
+
+                if (screenHandler == null) continue;
+
+                ScreenHandlerSyncHandler syncHandler = ((ScreenHandlerAccessor) screenHandler).getSyncHandler();
+                Slot slot = screenHandler.getSlot(entry.slot);
+
+                if (slot == null || slot.inventory != player.getInventory()) continue;
+
+                ItemStack stack = player.getInventory().getStack(slot.getIndex());
+
+                syncHandler.updateSlot(screenHandler, entry.slot, stack);
+            }
+
+            entries.clear();
+        });
+    }
+
+    private record Entry(ServerPlayNetworkHandler handler, int slot) {}
+}
